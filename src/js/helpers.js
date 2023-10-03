55a44f373fb82c717112845a5f35ca12bbad9cf8
rpmmohammadzadeh <126501574+rpmmohammadzadeh@users.noreply.github.com>
2023-10-03 16:22:04 -0400

    Update helpers.js

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -9,7 +9,7 @@ Pulsar.registerFunction(
     // Create array with all path segments and token name at the end
     const segments = [...tokenGroup.path];
     if (!tokenGroup.isRoot) {
-      segments.push(tokenGroup.name);
+      segments.push(tokenGroup.name.trim());
     }
     segments.push(token.name);
 
